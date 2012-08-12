8111a4b0f80255814de39b63d58c51c5d20a318d
Yosuke Ishikawa <ishkawa73@gmail.com>
2012-08-12 21:56:19 +0900

    set up submodules

--- ReadHub/RHAppDelegate.h
+++ ReadHub/RHAppDelegate.h
@@ -1,22 +1,7 @@
-//
-//  RHAppDelegate.h
-//  ReadHub
-//
-//  Created by Yosuke Ishikawa on 2012/08/12.
-//  Copyright (c) 2012年 Yosuke Ishikawa. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface RHAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
 
 @end
